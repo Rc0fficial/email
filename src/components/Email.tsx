5133d836bc1d0773143eed01885da8d52e3de93c
rc0fficial <to.arsalanali@gmail.com>
2023-10-25 02:16:54 -0700

    update

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -5,7 +5,7 @@ export const Email = () => {
     <div className="container mx-auto">
         <div className="flex items-center justify-center">
       <div className="flex items-center justify-center mt-10 cursor-pointer hover:bg-slate-100 rounded-lg shadow-lg px-4 py-4 max-w-[200px]">
-        <h1 className="text-[22px] ">Hello World</h1>
+        <h1 className="font-bold text-slate-600 text-[22px] ">Hello World</h1>
       </div>
       </div>
     </div>
