c8e962210a0726d4e8ca45742561e6609438dcfa
rc0fficial <to.arsalanali@gmail.com>
2023-10-25 02:13:05 -0700

    update

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -3,8 +3,10 @@ import React from "react";
 export const Email = () => {
   return (
     <div className="container mx-auto">
-      <div className="flex items-center justify-center mt-10  shadow-lg py-4">
-        <h1 className="text-[22px]">Hello World</h1>
+        <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center mt-10 cursor-pointer hover:bg-slate-100  shadow-lg px-4 py-4 max-w-[200px]">
+        <h1 className="text-[22px] ">Hello World</h1>
+      </div>
       </div>
     </div>
   );
