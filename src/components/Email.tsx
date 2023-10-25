44c476f4602fbd9d4bcff033fc6c9ae3246ab585
rc0fficial <to.arsalanali@gmail.com>
2023-10-25 02:01:02 -0700

    initial

---
+++ src/components/Email.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Email = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="flex items-center justify-center mt-10  shadow-lg py-4">
+        <h1 className="text-[22px]">Hello World</h1>
+      </div>
+    </div>
+  );
+};
